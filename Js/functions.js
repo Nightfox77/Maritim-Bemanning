bf9c1e9b4facb6211cbe73d072cd5cf33fc5535f
Michael Bäuml <102950224+Nightfox77@users.noreply.github.com>
2024-05-09 12:46:15 +0200

    hero

--- Js/functions.js
+++ Js/functions.js
@@ -1,11 +1,15 @@
 const burgerMenuIcon = document.querySelector("#menu");
 const modal = document.querySelector(".modal");
 const body = document.querySelector("body");
+const logo = document.querySelector(".logo")
+const hero = document.querySelector("#hero");
 
 
 burgerMenuIcon.onclick = function showModal() {
     modal.style.transform = "translateX(0%)";
     burgerMenuIcon.style.visibility = "hidden";
+    logo.style.visibility = "hidden";
+    hero.style.visibility = "hidden";
 }
 
 body.addEventListener("click", function hideModal(event) {
@@ -13,5 +17,10 @@ body.addEventListener("click", function hideModal(event) {
         console.log("close")
         burgerMenuIcon.style.visibility = "visible";
         modal.style.transform = "translateX(-100%)";
+        setTimeout(function() {
+            logo.style.visibility = "visible";
+            hero.style.visibility = "visible";
+        }, 500);
+       
     }
 })
